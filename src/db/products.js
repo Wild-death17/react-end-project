99e1fbc2636db5890251a1249c700b0ef956cca4
Wild_death17 <Moshe150519@Gmail.com>
2025-02-05 09:08:00 +0200

    addding lorem ipsum products data

---
+++ src/db/products.js
@@ -0,0 +1,9 @@
+export const products = [
+    { code: 1, name: "someItem", price: 123, image: "/images/someItem.jpg" },
+    { code: 2, name: "someItem", price: 123, image: "/images/someItem.jpg" },
+    { code: 3, name: "someItem", price: 123, image: "/images/someItem.jpg" },
+    { code: 4, name: "someItem", price: 123, image: "/images/someItem.jpg" },
+    { code: 5, name: "someItem", price: 123, image: "/images/someItem.jpg" },
+    { code: 6, name: "someItem", price: 123, image: "/images/someItem.jpg" },
+    { code: 6, name: "someItem", price: 123, image: "/images/someItem.jpg" },
+]
