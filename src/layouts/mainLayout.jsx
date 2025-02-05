8917b98c0ea217a7e95df839841a49796b84cb97
Wild_death17 <Moshe150519@Gmail.com>
2025-02-05 15:43:47 +0200

    layout final css design

--- src/layouts/mainLayout.jsx
+++ src/layouts/mainLayout.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/header";
 
 export default function MainLayout() {
   return (
-    <>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateRows: "50px auto",
+      }}>
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 }
