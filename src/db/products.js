24d4b1b6b8b3b68c0e16f4298dec85928182a58a
Wild_death17 <Moshe150519@Gmail.com>
2025-02-05 15:42:49 +0200

    added new data for better visualization

--- src/db/products.js
+++ src/db/products.js
@@ -1,9 +1,10 @@
 export const products = [
-    { code: 1, name: "someItem", price: 123, image: "/images/someItem.jpg" },
-    { code: 2, name: "someItem", price: 123, image: "/images/someItem.jpg" },
-    { code: 3, name: "someItem", price: 123, image: "/images/someItem.jpg" },
-    { code: 4, name: "someItem", price: 123, image: "/images/someItem.jpg" },
-    { code: 5, name: "someItem", price: 123, image: "/images/someItem.jpg" },
-    { code: 6, name: "someItem", price: 123, image: "/images/someItem.jpg" },
-    { code: 6, name: "someItem", price: 123, image: "/images/someItem.jpg" },
+    { code: 263674, name: "Access PS5", price: 379, image: "Access PS5" },
+    { code: 111800, name: "DualSense Charging Station", price: 129, image: "DualSense Charging Station" },
+    { code: 285274, name: "DualSense V2 PS5", price: 279, image: "DualSense V2 PS5" },
+    { code: 909495, name: "DualShock 4 V2 PS4", price: 239, image: "DualShock 4 V2 PS4" },
+    { code: 319444, name: "INZONE H5", price: 529, image: "INZONE H5" },
+    { code: 282950, name: "PlayStation Portable", price: 909, image: "PlayStation Portable" },
+    { code: 321996, name: "PS Gift Card", price: 500, image: "PS Gift Card" },
+    { code: 329088, name: "PS5 Slim Digital Edition 1TB", price: 1799, image: "PS5 Slim Digital Edition" },
 ]
