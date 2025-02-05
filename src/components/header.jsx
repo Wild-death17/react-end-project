231f47c56cbde8fba40647163cecfd4219c3da4d
Wild_death17 <Moshe150519@Gmail.com>
2025-02-05 09:04:34 +0200

    retwiking header design

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-
 export default function Header() {
-  let navigation = (
-    <nav>
-      <Link to="/manager">כניסה למנהל</Link>
-      <Link to="/store">דף הבית</Link>
-      <Link to="/shopingCart">עגלת קניות</Link>
-    </nav>
+  return (
+    <header>
+      <nav>
+        <Link to="manager">כניסה למנהל</Link>
+        <Link to="/">דף הבית</Link>
+        <Link to="shopingCart">עגלת קניות</Link>
+      </nav>
+    </header>
   );
-  return <>{navigation}</>;
 }
