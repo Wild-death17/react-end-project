ef97f82498d3655ad269c3494f551e5ce5ee2286
Wild_death17 <Moshe150519@Gmail.com>
2025-02-05 15:46:39 +0200

    header final design

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,16 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <nav>
+      <nav
+        style={{
+          height: "100%",
+          direction: "rtl",
+          display: "grid",
+          alignItems: "center",
+          gridTemplateColumns: "1fr auto auto",
+          gap: "30px",
+          padding: "0 20px",
+        }}>
         <Link to="manager">כניסה למנהל</Link>
         <Link to="/">דף הבית</Link>
         <Link to="shopingCart">עגלת קניות</Link>
