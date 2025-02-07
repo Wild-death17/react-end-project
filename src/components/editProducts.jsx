046a59a2d943003460b2d2391bda3b3b1b143518
Wild_death17 <Moshe150519@Gmail.com>
2025-02-07 17:46:23 +0200

    editing porduct component initial/(maybe last) design (pre css)

--- src/components/editProducts.jsx
+++ src/components/editProducts.jsx
@@ -1,3 +1,40 @@
+import { Form, useLoaderData } from "react-router-dom";
 export default function EditProduct() {
-  return <>Edit World!</>;
+  let product = useLoaderData() || {};
+
+  return (
+    <div>
+      <div>
+        <input placeholder="קוד המוצר" name="code" type="search" />
+        <button
+          onClick={(e) => {
+            /* reroute to /edit/:code */
+          }}>
+          חפש מוצר
+        </button>
+      </div>
+      <Form method="POST">
+        <input name="code" type="hidden" defaultValue={product.code} />
+        <input
+          defaultValue={product.name}
+          placeholder="שם המוצר"
+          name="name"
+          type="text"
+        />
+        <input
+          defaultValue={product.image}
+          placeholder="תמונת המוצר"
+          name="image"
+          type="text"
+        />
+        <input
+          defaultValue={product.price}
+          placeholder="מחיר המוצר"
+          name="price"
+          type="text"
+        />
+        <button className="primary">ערוך</button>
+      </Form>
+    </div>
+  );
 }
