beb5e73b7399cebf381c69ed4f09f6b763012b17
Wild_death17 <Moshe150519@Gmail.com>
2025-02-07 18:19:43 +0200

    styling done on editProduct component (still needs work on the search bar)

--- src/components/editProducts.jsx
+++ src/components/editProducts.jsx
@@ -1,39 +1,63 @@
 import { Form, useLoaderData } from "react-router-dom";
 export default function EditProduct() {
+  const formstyle = { padding: "20px", fontSize: "larger" };
+
   let product = useLoaderData() || {};
 
   return (
-    <div>
-      <div>
-        <input placeholder="קוד המוצר" name="code" type="search" />
+    <div
+      style={{
+        display: "flex",
+        margin: "auto",
+        flexDirection: "column",
+      }}>
+      <div style={{ margin: "30px" }}>
+        <input
+          style={formstyle}
+          placeholder="קוד המוצר"
+          name="code"
+          type="search"
+        />
         <button
+          style={formstyle}
           onClick={(e) => {
             /* reroute to /edit/:code */
           }}>
           חפש מוצר
         </button>
       </div>
-      <Form method="POST">
+      <Form
+        style={{
+          display: "flex",
+          margin: "auto",
+          flexDirection: "column",
+        }}
+        method="POST">
         <input name="code" type="hidden" defaultValue={product.code} />
         <input
+          style={formstyle}
           defaultValue={product.name}
           placeholder="שם המוצר"
           name="name"
           type="text"
         />
         <input
+          style={formstyle}
           defaultValue={product.image}
           placeholder="תמונת המוצר"
           name="image"
           type="text"
         />
         <input
+          style={formstyle}
           defaultValue={product.price}
           placeholder="מחיר המוצר"
           name="price"
           type="text"
         />
-        <button className="primary">ערוך</button>
+        <button style={formstyle} className="primary">
+          ערוך
+        </button>
       </Form>
     </div>
   );
