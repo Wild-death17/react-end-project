9108d0a59679a3a7caa569c90b9ed91828887689
Wild_death17 <Moshe150519@Gmail.com>
2025-02-07 18:18:42 +0200

    hopfully final styling for addProduct component

--- src/components/addProducts.jsx
+++ src/components/addProducts.jsx
@@ -1,12 +1,45 @@
 import { Form } from "react-router-dom";
 export default function AddProduct() {
+  const formstyle = { padding: "20px", fontSize: "larger" };
   return (
-    <Form method="POST">
-      <input placeholder="code" name="code" type="text" required />
-      <input placeholder="name" name="name" type="text" required />
-      <input placeholder="image" name="image" type="text" required />
-      <input placeholder="price" name="price" type="text" required />
-      <button className="primary">הוסף</button>
+    <Form
+      style={{
+        display: "flex",
+        margin: "auto",
+        flexDirection: "column",
+      }}
+      method="POST">
+      <input
+        style={formstyle}
+        placeholder="code"
+        name="code"
+        type="text"
+        required
+      />
+      <input
+        style={formstyle}
+        placeholder="name"
+        name="name"
+        type="text"
+        required
+      />
+      <input
+        style={formstyle}
+        placeholder="image"
+        name="image"
+        type="text"
+        required
+      />
+      <input
+        style={formstyle}
+        placeholder="price"
+        name="price"
+        type="text"
+        required
+      />
+      <button style={formstyle} className="primary">
+        הוסף
+      </button>
     </Form>
   );
 }
