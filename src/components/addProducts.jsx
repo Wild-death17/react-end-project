62a702d04c40ef3038054473f13fd836077477c1
Wild_death17 <Moshe150519@Gmail.com>
2025-02-07 17:39:50 +0200

    adding porduct component initial/(maybe last) design (pre css)

--- src/components/addProducts.jsx
+++ src/components/addProducts.jsx
@@ -1,3 +1,12 @@
+import { Form } from "react-router-dom";
 export default function AddProduct() {
-  return <>Add World!</>;
+  return (
+    <Form method="POST">
+      <input placeholder="code" name="code" type="text" required />
+      <input placeholder="name" name="name" type="text" required />
+      <input placeholder="image" name="image" type="text" required />
+      <input placeholder="price" name="price" type="text" required />
+      <button className="primary">הוסף</button>
+    </Form>
+  );
 }
