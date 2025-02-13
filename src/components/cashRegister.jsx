48b936b04a4ec400fc7451ab15c48252d020dd67
Wild_death17 <Moshe150519@Gmail.com>
2025-02-13 18:27:10 +0200

    payment page finak design (pre explicit css)

--- src/components/cashRegister.jsx
+++ src/components/cashRegister.jsx
@@ -1,13 +1,36 @@
 import { Form } from "react-router-dom";
-
+import { useCart } from "../contexts/cartContext";
 export default function CashRegister() {
+  const { displayCart, cartTotal } = useCart();
+
   return (
-    <Form action="">
-      <input placeholder="תעודת זהות" type="text" />
-      <br />
-      <input placeholder="כתובת למשלוח" type="text" />
-      <br />
-      <button>תשלום</button>
-    </Form>
+    <div>
+      {displayCart()}
+      <hr />
+      <h3>
+        סהכ:
+        {cartTotal()} ₪
+      </h3>
+      <hr />
+      <Form method="POST">
+        <input
+          className="form-tag"
+          name="id"
+          placeholder="תעודת זהות"
+          type="text"
+          required
+        />
+        <br />
+        <input
+          className="form-tag"
+          name="address"
+          placeholder="כתובת למשלוח"
+          type="text"
+          required
+        />
+        <br />
+        <button className="form-tag">תשלום</button>
+      </Form>
+    </div>
   );
 }
